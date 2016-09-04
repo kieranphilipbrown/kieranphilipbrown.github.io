b0275623bb496b9efca0c1d417ab3b9a4cf7f5e2
Kieran Brown <kieranpbrown@gmail.com>
2016-09-04 18:54:36 +0100

    Tidied script for all files.

--- js/index.js
+++ js/index.js
@@ -1,11 +1,4 @@
 $(document).ready(function($) {
-    $('.menu-btn').click(function(){
-        $('.main-menu').toggleClass('expand');
-    });
-    $('.accordion2').find('.accordion-toggle2').click(function(){
-        $(this).next().slideToggle('fast'); // Expand or collapse this panel
-        $(".accordion-content2").not($(this).next()).slideUp('fast'); // Hide the other panels
-    });
     $(".owl-carousel").owlCarousel({
         autoplay: true,
         autoplayHoverPause: true,
---
+++ js/site.js
@@ -0,0 +1,9 @@
+$(document).ready(function($) {
+    $('.menu-btn').click(function(){
+        $('.main-menu').toggleClass('expand');
+    });
+    $('.accordion2').find('.accordion-toggle2').click(function(){
+        $(this).next().slideToggle('fast'); // Expand or collapse this panel
+        $(".accordion-content2").not($(this).next()).slideUp('fast'); // Hide the other panels
+    });
+});
